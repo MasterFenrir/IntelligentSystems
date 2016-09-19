0d4856221ecf9a89a03673523feac090c9aacbd6
Wessel <w.j.becker@st.hanze.nl>
2016-09-19 10:49:06 +0200

    some changes by the prince of misscommits

--- Assignment1/Salesman/plotmean.m
+++ Assignment1/Salesman/plotmean.m
@@ -1,6 +1,6 @@
 n = 100;
 maxsteps = 200;
-temp = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5];
+temp = [0.5, 0.2 ,0.1 ,0.05 ,0.02 ,0.01];
 method = 1;
 
 meanresults = zeros(size(temp));
@@ -8,10 +8,14 @@ standarddeviationresults = zeros(size(temp));
 
 % Try the different temperatures
 for index = 1:length(temp)
-    results = tsp(n, maxsteps, temp(index), method);
-    meanresults(index) = mean(results);
-    standarddeviationresults(index) = sqrt(var(results));
+    results = tsp(n, maxsteps, temp(index), method);    
+    lastFifty = results(length(results) - 49:length(results));
+    meanresults(index) = mean(lastFifty);
+    standarddeviationresults(index) = sqrt(var(lastFifty));
+    fprintf('mean: %f \n', mean(lastFifty));
+    fprintf('varience: %f \n', var(lastFifty));
 end
+
 
 % Plot the results in a graph
 figure(3); semilogx(0,0); hold on;
--- Assignment1/Salesman/tsp.m
+++ Assignment1/Salesman/tsp.m
@@ -20,63 +20,63 @@
 
    close all;
 %  initialize random number generator and draw coordinates 
-   %rand('state',0); 
+%    rand('state',0); 
    cities = rand(n,2); 
    ord = [1:n];  op = path(ord,cities);
-
+    
    for jstep=1:ceil(maxsteps);
-   for ins = 1:100 
-      j = ceil(rand*n); len = ceil(rand*(n/2));
-      cand = reverse(ord,j,len);
-%  evaluate change of path length 
-      diff = delta(ord,cities,j,j+len);
-      np   = op + diff;
-%  met=1: threshold, met=2: metropolis
-      if ( (met==1 && (rand<exp(-diff/temp))||(diff<0)) || ...
-           (met==2 && diff<temp))
-         ord = cand;
-         op = np; 
-      end
-   end
-
-%  rescale length of path by sqrt(n) for output purposes
+       for ins = 1:100 
+          j = ceil(rand*n); len = ceil(rand*(n/2));
+          cand = reverse(ord,j,len);
+          %  evaluate change of path length 
+          diff = delta(ord,cities,j,j+len);
+          np   = op + diff;    
+          %  met=1: threshold, met=2: metropolis
+          if ( (met==1 && (rand<exp(-diff/temp))||(diff<0)) || ...
+               (met==2 && diff<temp))
+             ord = cand;
+             op = np; 
+          end
+        end
+       
+      %  rescale length of path by sqrt(n) for output purposes
       lt(jstep) =  op/sqrt(n);
       curlen = path(ord,cities)/sqrt(n);
-%  plot map, cities and path 
-      figure(1); plotcities(ord,cities);
-      title(['n =',num2str(n,'%3.0f'),       ...
-             '   t =',num2str(jstep*100,'%8.0f'),  ... 
-             '   l =',num2str(curlen,'%4.4f'),  ... 
-             '   T =',num2str(temp,'%6.6f')],   ...
-             'fontsize',16);
-    if (met==1) 
-        xlabel(['Metropolis algorithm, annealing'],'fontsize',16);
-    else 
-        xlabel(['Threshold algorithm', ...
-                '    T(0)=',num2str(temps,'%4.4f')], ...
-                'fontsize',16);
-    end
-      %pause(0.1);
+      %  plot map, cities and path 
+%       figure(1); plotcities(ord,cities);
+%       title(['n =',num2str(n,'%3.0f'),       ...
+%              '   t =',num2str(jstep*100,'%8.0f'),  ... 
+%              '   l =',num2str(curlen,'%4.4f'),  ... 
+%              '   T =',num2str(temp,'%6.6f')],   ...
+%              'fontsize',16);
+%     if (met==1) 
+%         xlabel(['Metropolis algorithm, annealing'],'fontsize',16);
+%     else 
+%         xlabel(['Threshold algorithm', ...
+%                 '    T(0)=',num2str(temps,'%4.4f')], ...
+%                 'fontsize',16);
+%     end
+%     pause(0.1);
    end
-%  plot evolution of length versus iteration step
-      figure(2); plot(0,0); hold on; 
-      plot(tt,lt,'k.'); 
-      title(['n =',num2str(n,'%3.0f'),       ...
+   %  plot evolution of length versus iteration step
+   figure(2); plot(0,0); hold on; 
+   plot(tt,lt,'k.'); 
+   title(['n =',num2str(n,'%3.0f'),       ...
              '   l =',num2str(curlen,'%4.4f'),  ... 
              '   T =',num2str(temps,'%4.4f')],   ... 
              'fontsize',16);
-      if (met==1) 
-         xlabel(['Metropolis steps / 100'],'fontsize',16);
-      else 
-         xlabel(['Threshold steps /100'],'fontsize',16);
-      end
-         ylabel(['l'],'fontsize',16);
-    
-    meanstart = maxsteps - 49;
-    if (meanstart < 1)
-        meanstart = 1;
+    if (met==1) 
+        xlabel(['Metropolis steps / 100'],'fontsize',16);
+    else 
+        xlabel(['Threshold steps /100'],'fontsize',16);
     end
-    lastfifty = lt(meanstart:maxsteps);
-    fprintf('The mean of the last fifty elements is %f\n', mean(lastfifty));
-    fprintf('The variance of the last fifty elements is %f\n', var(lastfifty));
+        ylabel(['l'],'fontsize',16);
+    
+%     meanstart = maxsteps - 49;
+%     if (meanstart < 1)
+%         meanstart = 1;
+%     end
+%     lastfifty = lt(meanstart:maxsteps);
+%     fprintf('The mean of the last fifty elements is %f\n', mean(lastfifty));
+%     fprintf('The variance of the last fifty elements is %f\n', var(lastfifty));
     
