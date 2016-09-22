d3d6488cbb291cd2ecf38793f8a529dde2dcb170
MasterFenrir <masterfenrir@outlook.com>
2016-09-22 14:26:11 +0200

    Added structure in comments

--- Assignment2/VQ/main.m
+++ Assignment2/VQ/main.m
@@ -7,6 +7,16 @@ load('w6_1z.mat');
 [zP, zN] = size(w6_1z);
 
 K = 3;
+n = 0.1;
 mu = 'constantProduct';
 tMax = 2;
 FEVAL()
+
+% Initialize the prototypes by selecting random data points
+% From 1 to tMax:
+%   Shuffle data
+%   for each datapoint, compare to prototypes
+%   Protype that is closest gets updated by n
+%   Plot datapoints
+%   Evaluate the quantization error Hvq
+% Plot the quantization error as a function of t
