5a290cb5e65f31801e9e320bf9b0cca5bb0b86b1
MasterFenrir <masterfenrir@outlook.com>
2016-10-07 15:30:06 +0200

    I don't remember

--- Assignment4/matlab/main.m
+++ Assignment4/matlab/main.m
@@ -1,7 +1,7 @@
 normdist = load('normdist(1).mat');
 
 % plotScatter(normdist.S1, normdist.S2, normdist.T);
-% plotNormalDistributions(normdist.S1, normdist.S2);
+ plotNormalDistributions(normdist.S1, normdist.S2);
 
 [size1, ~] = size(normdist.S1);
 [size2, ~] = size(normdist.S2);
