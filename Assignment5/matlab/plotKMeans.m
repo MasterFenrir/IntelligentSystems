23d2a427903263d510331d4fc4aa638ebd09d771
Wessel <w.j.becker@st.hanze.nl>
2016-10-23 15:12:37 +0200

    k-means imgases and plot saving code

--- Assignment5/matlab/plotKMeans.m
+++ Assignment5/matlab/plotKMeans.m
@@ -1,13 +1,16 @@
-function [  ] = plotKMeans( means, clusters )
+function [  ] = plotKMeans( means, clusters, name )
 %PLOTKMEANS Summary of this function goes here
 %   Detailed explanation goes here
     colours = {'k', 'm', 'b', 'r', 'g', [0, 0.9, 0.9] , [0.67, 0.45, 0.22], [1, 0.55 0.1]};
+    k = length(means);
+    fullName = strcat('img/', name, num2str(k));
+    
     iptsetpref('ImshowBorder','tight');
-    figure(2);
+    figure('name', fullName);
     hold off; box on; 
     axis square; hold on;
     
-    for i = 1:length(means)
+    for i = 1:k
         cluster = clusters{i};
         s1 = scatter(cluster(:, 1), cluster(:, 2), 30, '.' );
         s2 = scatter(means(i, 1), means(i, 2), 150, 'filled', 'p');
@@ -15,7 +18,9 @@ function [  ] = plotKMeans( means, clusters )
         s2.MarkerFaceColor = s1.MarkerEdgeColor;
     end
 
-    ylabel(['Length in cm'],'fontsize',16);
-    xlabel(['Hair length in cm'],'fontsize',16);
+    ylabel(['y'],'fontsize',16);
+    xlabel(['x'],'fontsize',16);
+    
+    print(fullName, '-dpng')
 end
 
