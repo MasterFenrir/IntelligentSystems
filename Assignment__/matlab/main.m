665a56e5e2ef6b02f4ccc21c70ee93e786e21e7f
Wessel <w.j.becker@st.hanze.nl>
2016-10-31 19:12:09 +0100

    added some text

--- Assignment__/matlab/main.m
+++ Assignment__/matlab/main.m
@@ -2,12 +2,14 @@ normdist = load('normdist(1).mat');
 
 %  plotScatter(normdist.S1, normdist.S2, normdist.T);
 %  plotNormalDistributions(normdist.S1, normdist.S2);
-  plotPosteriorProbability(normdist.S1, normdist.S2, normdist.T);
+%   plotPosteriorProbability(normdist.S1, normdist.S2, normdist.T);
 
 
 
 % decisionBoundarySolver(normdist.S1, normdist.S2);
-% treeThing = load('dataAEX.mat');
-% treeLabels = load('labelsAEX.mat');
-% tree = linkage(treeThing.data);
-%  dendrogram(tree, 'Labels', treeLabels.labels);
+
+treeThing = load('dataAEX.mat');
+treeLabels = load('labelsAEX.mat');
+% dists = pdist(treeThing.data);
+tree = linkage(treeThing.data, 'complete');
+dendrogram(tree, 'Labels', treeLabels.labels);
